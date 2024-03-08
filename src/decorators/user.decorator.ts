21fb7a389d6c818801b3e343e3ebfd7f15a52e46
Lucas Machado <lukaasmachado143@gmail.com>
2024-03-08 10:13:09 -0300

    feat: filters in decorators.

--- src/decorators/user.decorator.ts
+++ src/decorators/user.decorator.ts
@@ -1,8 +1,13 @@
 import { ExecutionContext, NotFoundException, createParamDecorator } from "@nestjs/common";
 
-export const User = createParamDecorator((_: unknown, context: ExecutionContext) => {
+export const User = createParamDecorator((filter: string, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest()
 
-    if (request.user) return request.user
-    else throw new NotFoundException('Usuário não encontrado no Request!');
+    if (request.user) {
+        if (filter) {
+            return request.user[filter]
+        }
+        else return request.user
+    }
+    else throw new NotFoundException('Usuário não encontrado no Request. Use o AuthGuard para obter o usuário !');
 })
